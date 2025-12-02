e4fd6841e72a72fb039412328b04b9216e39b1c6
Muhammad-Munsif <m.munsif4375@gmail.com>
2025-12-02 23:58:39 +0500

    To create files and changes into button jsx

--- src/components/common/LoadingSpinner.jsx
+++ src/components/common/LoadingSpinner.jsx
@@ -1,19 +1,19 @@
 // src/components/common/LoadingSpinner.jsx
-import React from 'react';
+import React from "react";
 
 const LoadingSpinner = ({
   size = "md",
   color = "blue",
   fullScreen = false,
   message = "Loading...",
-  showMessage = false
+  showMessage = false,
 }) => {
   const sizeClasses = {
     xs: "w-4 h-4",
     sm: "w-6 h-6",
     md: "w-8 h-8",
     lg: "w-12 h-12",
-    xl: "w-16 h-16"
+    xl: "w-16 h-16",
   };
 
   const colorClasses = {
@@ -22,7 +22,7 @@ const LoadingSpinner = ({
     white: "border-white",
     green: "border-green-600",
     red: "border-red-600",
-    yellow: "border-yellow-600"
+    yellow: "border-yellow-600",
   };
 
   const spinner = (
@@ -31,7 +31,7 @@ const LoadingSpinner = ({
         <div
           className={`${sizeClasses[size]} border-2 ${colorClasses[color]} border-t-transparent rounded-full animate-spin`}
         />
-        {size === 'xl' && (
+        {size === "xl" && (
           <div className="absolute inset-0 border-2 border-gray-200 rounded-full"></div>
         )}
       </div>
@@ -58,13 +58,13 @@ export const InlineLoadingSpinner = ({ size = "sm", color = "blue" }) => {
   const sizeClasses = {
     xs: "w-3 h-3",
     sm: "w-4 h-4",
-    md: "w-5 h-5"
+    md: "w-5 h-5",
   };
 
   const colorClasses = {
     blue: "border-blue-600",
     gray: "border-gray-600",
-    white: "border-white"
+    white: "border-white",
   };
 
   return (
